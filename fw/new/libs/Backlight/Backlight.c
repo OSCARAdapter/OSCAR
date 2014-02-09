e3aeb8d016f5d8aa1eaefbda4f69cac4bffc1dda
Ali Lown <ali@lown.me.uk>
2014-02-09 04:18:06 +0000

    Implement backlight up/down

--- fw/new/libs/Backlight/Backlight.c
+++ fw/new/libs/Backlight/Backlight.c
@@ -2,29 +2,50 @@
 #include <EEPROM.h>
 #include <Backlight.h>
 
-void Backlight::init(int pin)
+void Backlight::init(int pin, int eeprom)
 {
   pinMode(pin, OUTPUT);
   this.pin = pin;
+  this.eeprom = eeprom;
+  current = levels[0];
 }
 
 void Backlight::set(int level)
 {
   analogWrite(pin, level);
-  EEPROM.write(0, level);
+  current = level;
+  EEPROM.write(eeprom, level);
 }
 
 int Backlight::get()
 {
-  return EEPROM.read(0);
+  return current;
 }
 
 void Backlight::up()
 {
-
+  int nextStep = findNearestStep(this.get())++;
+  if(nextStep < NUM_LEVELS)
+    this.set(levels[nextStep]);
 }
 
 void Backlight::down()
 {
+  int nextStep = findNearestStep(this.get())--;
+  if(nextStep >= 0)
+    this.set(levels[nextStep]);
+}
 
+int Backlight::findNearestStep(int level)
+{
+  if(level > levels[NUM_LEVELS-2])
+    return NUM_LEVELS-1;
+
+  for(int i=0; i<NUM_LEVELS-1; i++)
+  {
+    if((levels[i] =< level) && (levels[i+1] > level))
+      return i;
+  }
+
+  return 0;
 }
--- fw/new/libs/Backlight/Backlight.h
+++ fw/new/libs/Backlight/Backlight.h
@@ -4,7 +4,7 @@
 class Backlight
 {
   public:
-    void init(int pin);
+    void init(int pin, int eeprom = 0);
     void set(int level);
     int get();
 
@@ -12,7 +12,14 @@ class Backlight
     void down();
 
   private:
+    int findNearestStep(int level);
+
     int pin;
+    int eeprom;
+    int current;
+
+    //Levels chosen by careful observation
+#define NUM_LEVELS 17
     int levels[] = {0, 1, 2, 3, 4, 6, 8, 12, 16, 23,
                     32, 45, 64, 90, 128, 180, 255};
 };
