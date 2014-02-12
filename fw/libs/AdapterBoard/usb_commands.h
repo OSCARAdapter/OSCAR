f17a48d1f85e2f23638ba42ab1915c2757acd39b
Ali Lown <ali@lown.me.uk>
2014-02-12 22:21:27 +0000

    Declare all supported USB commands

---
+++ fw/libs/AdapterBoard/usb_commands.h
@@ -0,0 +1,17 @@
+#ifndef USB_COMMANDS_H
+#define USB_COMMANDS_H
+
+#define CMD_ACK          0xAF
+#define CMD_RESP         0xBF
+
+#define CMD_BL_ON        0x10
+#define CMD_BL_OFF       0x11
+#define CMD_BL_LEVEL     0x12
+#define CMD_BL_UP        0x13
+#define CMD_BL_DOWN      0x14
+#define CMD_BL_GET_STATE 0x1F
+
+#define CMD_RGB_SET      0x20
+#define CMD_RGB_GET      0x2F
+
+#endif
