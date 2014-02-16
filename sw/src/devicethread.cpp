d0570b2ed8846bfd722a661d8d1d8082eebd65f6
Ali Lown <ali@lown.me.uk>
2014-02-16 17:04:55 +0000

    Define the devicethread

--- sw/src/devicethread.cpp
+++ sw/src/devicethread.cpp
@@ -2,8 +2,56 @@
 
 DeviceThread::DeviceThread()
 {
+  exit = false;
+}
+
+void DeviceThread::finish()
+{
+  exit = true;
 }
 
 DeviceThread::run()
 {
+  int ret;
+  while(!exit)
+  {
+    if(!board.isConnected())
+    {
+      ret = board.attachDevice();
+      if(ret < 0)
+      {
+        //Failed to attach
+        wait(3000);
+        notConnected();
+        continue;
+      }
+      else
+        connected();
+    }
+
+    //Connected, do actual work in this thread...
+
+    //Update status
+    getBacklightState();
+
+    //Process command queue
+    while(!cmd.isEmpty())
+    {
+      Command_t c = cmd.dequeue();
+      if(!IS_GET(c.cmd))
+        board.sendCmd(c.cmd, c.arg1, c.arg2, c.arg3);
+      else
+      {
+        //TODO:
+      }
+    }
+
+    //Wait for some time period. Number chosen at random as prime.
+    wait(635);
+  }
+}
+
+void DeviceThread::enqueue(const Command_t & c)
+{
+  cmd.enqueue(c);
 }
--- sw/src/devicethread.h
+++ sw/src/devicethread.h
@@ -2,6 +2,16 @@
 #define DEVICETHREAD_H
 
 #include <QThread>
+#include <QQueue>
+#include "boardv1.h"
+
+typedef struct Command
+{
+  uint8_t cmd;
+  uint8_t arg1;
+  uint8_t arg2;
+  uint8_t arg3;
+} Command_t;
 
 class DeviceThread : public QThread
 {
@@ -10,8 +20,29 @@ class DeviceThread : public QThread
   public:
     DeviceThread();
 
+    void setBacklightPower(bool on);
+    void setBacklightLevel(int level);
+    void getBacklightState();
+
+  slot:
+    void finish();
+    void enqueue(const Command_t & c);
+
+  signals:
+    //Called whenever a change in connection state is noticed
+    void connected();
+    void notConnected();
+
+    //Called as a response to 'GET' commands
+    void backlightResponse(bool on, int level);
+
   protected:
     void run();
+
+  private:
+    BoardV1 board;
+    bool exit;
+    QQueue<Command_t> cmd;
 };
 
 #endif
