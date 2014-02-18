ffd5d093b2807b1f9a4fba5f1bf952ef903c2eb6
Ali Lown <ali@lown.me.uk>
2014-02-18 00:16:46 +0000

    Remove bodge code

--- sw/src/mainwindow.cpp
+++ sw/src/mainwindow.cpp
@@ -56,42 +56,8 @@ void MainWindow::togglePower()
   }
 }
 
-/*-----------------BODGE BEGIN---------------------------- */
-#define NUM_LEVELS 17
-const int levels[] = {0, 1, 2, 3, 4, 6, 8, 12, 16, 23,
-                      32, 45, 64, 90, 128, 180, 255};
-int bodgeFindNearestStep(int level)
-{
-  if(level > levels[NUM_LEVELS-2])
-    return NUM_LEVELS-1;
-
-  for(int i=0; i<NUM_LEVELS-1; i++)
-  {
-    if((levels[i] <= level) && (levels[i+1] > level))
-      return i;
-  }
-
-  return 0;
-}
-
-void bodgeIncrease(Ui::MainWindow ui)
-{
-  int curStep = bodgeFindNearestStep(ui.backlightSlider->value());
-  if((curStep+1) < NUM_LEVELS)
-    ui.backlightSlider->setValue(levels[curStep+1]);
-}
-
-void bodgeDecrease(Ui::MainWindow ui)
-{
-  int curStep = bodgeFindNearestStep(ui.backlightSlider->value());
-  if((curStep-1) >= 0)
-    ui.backlightSlider->setValue(levels[curStep-1]);
-}
-/*-----------------BODGE END------------------------------ */
-
 void MainWindow::increaseBacklight()
 {
-  bodgeIncrease(ui);
   Command_t cmd;
   cmd.cmd = CMD_BL_UP;
   dev->enqueue(cmd);
@@ -99,7 +65,6 @@ void MainWindow::increaseBacklight()
 
 void MainWindow::decreaseBacklight()
 {
-  bodgeDecrease(ui);
   Command_t cmd;
   cmd.cmd = CMD_BL_DOWN;
   dev->enqueue(cmd);
